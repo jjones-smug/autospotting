4d282e83e9064828c23a4f3d9d84069a224d5cd1
Jim Jones <jjones@smugmug.com>
2018-08-15 11:59:02 -0700

    Bracket spot launch call with code to check ASG status (min OD check)

--- autospotting.go
+++ autospotting.go
@@ -8,8 +8,8 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws/endpoints"
-	"github.com/blueharvest/autospotting/core"
-	"github.com/blueharvest/ec2-instances-info"
+	"github.com/jjones-smug/autospotting/core"
+	"github.com/jjones-smug/ec2-instances-info"
 	"github.com/namsral/flag"
 )
 
