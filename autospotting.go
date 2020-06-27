a54fe3dbf3daa5648a378bab8ff6cc726d9a2a1a
Jim Jones <jjones@smugmug.com>
2020-06-27 12:22:31 -0700

    More go compiler errors...

--- autospotting.go
+++ autospotting.go
@@ -10,7 +10,7 @@ import (
 	"os"
 
 	// autospotting "github.com/AutoSpotting/AutoSpotting/core"
-	autospotting "github.com/jjones-smug/autospotting"
+	// autospotting "github.com/jjones-smug/autospotting"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
